ffdca803e5076f155c35e923f70205027c72b8c2
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-16 00:03:24 +0900

    useEffect활용 Cart 애니메이션 구현

--- Project06/src/components/Layout/HeaderCartButton.js
+++ Project06/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighlighted, seBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItmes = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItmes = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    seBtnIsHighlighted(true);
+
+    setTimeout(() => {
+      seBtnIsHighlighted(false);
+    }, 300);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
