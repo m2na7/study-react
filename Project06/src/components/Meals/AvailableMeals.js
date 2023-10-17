e87ae8dfc773b9bb978f41626db85aeb3f541b99
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-18 00:54:30 +0900

    결제 양식 및 유효성 검사 추가

--- Project06/src/components/Meals/AvailableMeals.js
+++ Project06/src/components/Meals/AvailableMeals.js
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
-import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState(null); // null 생략가능
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http2-a8744-default-rtdb.firebaseio.com/meals"
+        "https://react-http2-a8744-default-rtdb.firebaseio.com/meals.json"
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong ! ");
+        throw new Error("Something went wrong!");
       }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -34,12 +36,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals()
-      .then()
-      .catch((error) => {
-        setIsLoading(false);
-        setHttpError(error.message);
-      });
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []); // 의존성 X -> 컴포넌트가 처음 로딩시에만 실행됨
 
   if (isLoading) {
@@ -53,15 +53,15 @@ const AvailableMeals = () => {
   if (httpError) {
     return (
       <section className={classes.MealsError}>
-        <p>Failed to fetch</p>
+        <p>{httpError}</p>
       </section>
     );
   }
 
   const mealsList = meals.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
