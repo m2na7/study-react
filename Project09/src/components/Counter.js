5fc8d96a2dbf4725662c2248ec00473ac52760a7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-19 04:52:31 +0900

    useDispatch()활용해 action Dispatch

--- Project09/src/components/Counter.js
+++ Project09/src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -6,6 +6,15 @@ const Counter = () => {
   //  react-redux는 이 컴포넌트를 위해 리덕스 저장소에  자동으로 구독을 설정,
   //  리덕스 저장소에서 데이터가 변경될 때마다 자동으로 업데이트되고 최신 카운터를 받게된다.
   const counter = useSelector((state) => state.counter);
+  // Redux store에 대한 action을 보냄
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -13,6 +22,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
