90f3a148f9621289ee8ed290f08be725ed09ef52
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-21 19:34:24 +0900

    redux 활용 장바구니 기능 구현

--- Project10/src/components/Shop/ProductItem.js
+++ Project10/src/components/Shop/ProductItem.js
@@ -1,8 +1,23 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        // id: id (프로퍼티 이름을 적어두면 자동으로 뒤에 확장된다. 생략가능하다는 뜻)
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +28,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
