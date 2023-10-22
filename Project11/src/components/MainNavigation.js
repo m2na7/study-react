47fb8e26af0fbce057d3788beb06cf595200b329
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-22 21:09:46 +0900

    NavLink
    현재 활성된 라우트 네비게이션 버튼 활성화

--- Project11/src/components/MainNavigation.js
+++ Project11/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
@@ -8,10 +8,26 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/ ">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // "/" 로 시작하는 모든 url에 활성화 되기 때문에 end 표시
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
