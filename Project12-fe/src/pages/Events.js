ffe92b80de4bc1c4b1005061c0db19fedbb000c7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-26 19:11:03 +0900

    json()

--- Project12-fe/src/pages/Events.js
+++ Project12-fe/src/pages/Events.js
@@ -14,9 +14,10 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
