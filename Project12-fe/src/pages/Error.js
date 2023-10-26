3cf9e6044661ad9d0c7117af51ffce6160af4d3e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-26 19:08:05 +0900

    useRouteError()
    오류 데이터 추출 및 응답 내보내기

--- Project12-fe/src/pages/Error.js
+++ Project12-fe/src/pages/Error.js
@@ -1,5 +1,25 @@
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+
 function ErrorPage() {
-  return <h1>An error occurred !</h1>;
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+  return (
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+  );
 }
 
 export default ErrorPage;
--- Project12-fe/src/pages/Events.js
+++ Project12-fe/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -14,7 +14,9 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
     return resData.events;
