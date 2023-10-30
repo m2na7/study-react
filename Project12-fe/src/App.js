8ac6572d6c1bb06f7a92fc8fcd68cf36d4f5835c
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-10-31 01:39:02 +0900

    회원가입 로직 구현

--- Project12-fe/src/App.js
+++ Project12-fe/src/App.js
@@ -5,6 +5,7 @@ import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
+  action,
 } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -13,7 +14,9 @@ import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -58,6 +61,7 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: "newsletter",
