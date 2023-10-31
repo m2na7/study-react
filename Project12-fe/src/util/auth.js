716c53ea3aa9b90d07152197f4e7c30db9f4c25e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-11-01 01:34:37 +0900

    인증 상태에 따른 라우트 보호
    비로그인 상태에서 페이지 접근시 회원가입 페이지로 redirect

--- Project12-fe/src/util/auth.js
+++ Project12-fe/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
   return token;
@@ -5,4 +7,13 @@ export function getAuthToken() {
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+     alert("로그인 후 이용 가능합니다.");
+    return redirect("/auth");
+  }
 }
