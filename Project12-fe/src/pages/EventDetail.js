1ed9932f437f0c0c5eaa2d3a304a11d37020f97a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-11-06 05:12:06 +0900

    fix delete event

--- Project12-fe/src/pages/EventDetail.js
+++ Project12-fe/src/pages/EventDetail.js
@@ -1,27 +1,27 @@
-import { Suspense } from "react";
+import { Suspense } from 'react';
 import {
-  Await,
-  defer,
+  useRouteLoaderData,
   json,
   redirect,
-  useRouteLoaderData,
-} from "react-router-dom";
+  defer,
+  Await,
+} from 'react-router-dom';
 
-import EventItem from "../components/EventItem";
-import EventsList from "../components/EventsList";
-import { getAuthToken } from "../util/auth";
+import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
+import { getAuthToken } from '../util/auth';
 
 function EventDetailPage() {
-  const { event, events } = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData('event-detail');
 
   return (
     <>
-      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading ...</p>}>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
         <Await resolve={event}>
           {(loadedEvent) => <EventItem event={loadedEvent} />}
         </Await>
       </Suspense>
-      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading ...</p>}>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
         <Await resolve={events}>
           {(loadedEvents) => <EventsList events={loadedEvents} />}
         </Await>
@@ -33,10 +33,11 @@ function EventDetailPage() {
 export default EventDetailPage;
 
 async function loadEvent(id) {
-  const response = await fetch("http://localhost:8080/events/" + id);
+  const response = await fetch('http://localhost:8080/events/' + id);
+
   if (!response.ok) {
     throw json(
-      { message: "Could not fetch details for selected events." },
+      { message: 'Could not fetch details for selected event.' },
       {
         status: 500,
       }
@@ -48,7 +49,7 @@ async function loadEvent(id) {
 }
 
 async function loadEvents() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
@@ -56,7 +57,7 @@ async function loadEvents() {
     //   status: 500,
     // });
     throw json(
-      { message: "Could not fetch events." },
+      { message: 'Could not fetch events.' },
       {
         status: 500,
       }
@@ -78,21 +79,22 @@ export async function loader({ request, params }) {
 
 export async function action({ params, request }) {
   const eventId = params.eventId;
-  const token = getAuthToken;
-  const response = await fetch("http://localhost:8080/events/" + eventId, {
-    // request.method => delete (EventItem) 하드코딩 하지 않는 습관 !
+
+  const token = getAuthToken();
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
     method: request.method,
     headers: {
-      Authorization: "Bearer" + token,
-    },
+      'Authorization': 'Bearer ' + token
+    }
   });
+
   if (!response.ok) {
     throw json(
-      { message: "Could not delete event." },
+      { message: 'Could not delete event.' },
       {
         status: 500,
       }
     );
   }
-  return redirect("/events");
+  return redirect('/events');
 }
