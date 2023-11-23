dd67d96fea1769ec147a3589e955b0e754ef9909
Minha Kang <118591632+m2na7@users.noreply.github.com>
2023-11-23 23:45:35 +0900

    리스트 애니메이션
    - 스태거링 효과 및 키 프레임 효과

--- Project15/src/components/NewChallenge.jsx
+++ Project15/src/components/NewChallenge.jsx
@@ -57,12 +57,18 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul
+          id="new-challenge-images"
+          variants={{
+            visible: { transition: { staggerChildren: 0.05 } },
+          }}
+        >
           {images.map((image) => (
             <motion.li
               variants={{
+                // 50 % -> 80% -> 130% -> 100% 순서
                 hidden: { opacity: 0, scale: 0.5 },
-                visible: { opacity: 1, scale: 1 },
+                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
               }}
               exit={{ opacity: 1, scale: 1 }}
               transition={{ type: "spring" }}
@@ -73,7 +79,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
