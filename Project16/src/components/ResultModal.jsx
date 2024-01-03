31b6fee66be0de69bfa2f8ea026d6c929cf28264
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-01-03 19:09:50 +0900

    forwardRef
    - Refs값을 다른 컴포넌트로 전달

---
+++ Project16/src/components/ResultModal.jsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+
+// ref 값을 다른 컴포넌트로 전달하기 위해서는 forwardRef를 사용해야 한다.
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  return (
+    <dialog ref={ref} className="result-modal" open>
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime}</strong> seconds.
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
--- Project16/src/components/TimerChallenge.jsx
+++ Project16/src/components/TimerChallenge.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import { useState } from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef(); // 모달 backdrop 설정을 위해 추가
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -10,6 +12,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal(); // 내장 dialog 요소 (표준 브라우저 기능)
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -20,20 +23,23 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost !</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? "active" : undefined}>
-        {timerStarted ? "Time is running..." : "Timer inactive"}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost !</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
