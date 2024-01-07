fbdc26cb860acc080951c38845d8da2e1666c091
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-01-08 02:40:13 +0900

    ref로 사용자 입력 받아오기

--- Project17/src/components/NewProject.jsx
+++ Project17/src/components/NewProject.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
 import Input from "./input";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,16 +27,19 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="제목" />
+        <Input type="text" ref={title} label="제목" />
         {/* textarea는 자동으로 true로 설정됨 */}
-        <Input label="설명" textarea />
-        <Input label="마감일" />
+        <Input ref={description} label="설명" textarea />
+        <Input type="date" ref={dueDate} label="마감일" />
       </div>
     </div>
   );
