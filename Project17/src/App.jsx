2dd607301b58471e529ebb771b51be1cf11dfc5b
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-01-07 21:49:00 +0900

    상태관리를 통해 컴포넌트 간 교환

--- Project17/src/App.jsx
+++ Project17/src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoPojectSelected from "./components/NoPojectSelected";
 import ProjectSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        // undefined -> 선택된 프로젝트 X, null -> 새로운 프로젝트 추가
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoPojectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoPojectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- Project17/src/components/ProjectsSidebar.jsx
+++ Project17/src/components/ProjectsSidebar.jsx
@@ -1,15 +1,13 @@
 import Button from "./Button";
 
-export default function ProjectSidebar() {
+export default function ProjectSidebar({ onStartAddProject }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-          <Button>
-              + 프로젝트 추가
-          </Button>
+        <Button onClick={onStartAddProject}>+ 프로젝트 추가</Button>
       </div>
       <ul></ul>
     </aside>
