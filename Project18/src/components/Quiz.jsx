b716273582242c424970bfe48c31e8f16bf648c4
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-02-06 19:15:21 +0900

    답변 셔플 & 퀴즈 완료 로직

--- Project18/src/components/Quiz.jsx
+++ Project18/src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import QUESTIONS from "../question.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +14,25 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="트로피 아이콘" />
+        <h2>퀴즈가 끝났습니다!</h2>
+      </div>
+    );
+  }
+
+  // 답변의 복사본 -> 기존 답변구조를 해치지 않기위함 (answers의 첫번째 요소가 항상 정답이라서.)
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
