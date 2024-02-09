574d3950bb149f9875468c5ce615a1ea7beeb06b
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-02-09 22:54:38 +0900

    선택된 담변 기반 타이머 설정

--- Project18/src/components/Question.jsx
+++ Project18/src/components/Question.jsx
@@ -10,6 +10,15 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     isCorrect: null,
   });
 
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -24,7 +33,7 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
 
       setTimeout(() => {
         onSelectAnswer(answer);
-      }, 1000);
+      }, 2000);
     }, 1000);
   }
 
@@ -38,7 +47,12 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer} // 키 설정 -> timer값이 바뀔 때, QuestionTimer 컴포넌트를 삭제하고 다시 재생성
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
